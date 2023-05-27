e2e2970d9a4f1cb4e77d7cb4f1870ad545df8b8d
mcruffel <hector.sanchezt@pragma.com.co>
2023-05-27 13:38:58 -0500

    commitv1

---
+++ src/main/java/com/agenciacristal/crud/product/ProductController.java
@@ -0,0 +1,48 @@
+package com.agenciacristal.crud.product;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+import java.util.Objects;
+
+@RestController
+@RequestMapping(path = "api/v1/products")
+public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService ){
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> getProducts(){
+        return productService.getProducts();
+    }
+    @PostMapping
+    public ResponseEntity<Object> registrarProducto(@RequestBody Product product){
+        return this.productService.newProduct(product);
+    }
+
+    @PutMapping
+    public ResponseEntity<Object> actualizarProducto(@RequestBody Product product){
+        try{
+            return this.productService.newProduct(product);
+        }catch (Exception e){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "no existe ese producto");
+        }
+
+    }
+
+    @DeleteMapping(path="{productId}")
+    public ResponseEntity<Object> eliminarProducto(@PathVariable ("productId")Long id){
+        return this.productService.deleteProduct(id);
+    }
+
+
+}
---
+++ src/main/java/com/agenciacristal/crud/product/ProductService.java
@@ -0,0 +1,66 @@
+package com.agenciacristal.crud.product;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+    HashMap<String,Object> datos;
+    private final ProductRepository productRepository;
+    @Autowired
+    public ProductService(ProductRepository productRepository){
+        this.productRepository = productRepository;
+    }
+    public List<Product> getProducts(){
+        return this.productRepository.findAll();
+    }
+    public ResponseEntity<Object> newProduct(Product product) {
+        Optional<Product> res = productRepository.findProductByName(product.getName());
+        datos=new HashMap<>();
+
+        if(res.isPresent()&& product.getId()==null){
+            datos.put("error", true);
+            datos.put("menssage","ya existe un producto con ese nombre");
+            return new ResponseEntity<>(
+                    datos,
+                    HttpStatus.CONFLICT
+            );
+        }
+        datos.put("menssage","se guardó con exito");
+        if(product.getId()!=null){
+            datos.put("menssage","se actualizó con exito");
+        }
+        productRepository.save(product);
+        datos.put("data", product);
+        return new ResponseEntity<>(
+                datos,
+                HttpStatus.CREATED
+        );
+    }
+
+    public ResponseEntity<Object> deleteProduct(Long id){
+        datos=new HashMap<>();
+        boolean existe=this.productRepository.existsById(id);
+        if(!existe){
+            datos.put("error", true);
+            datos.put("menssage","no existe un producto con ese id");
+            return new ResponseEntity<>(
+                    datos,
+                    HttpStatus.CONFLICT
+            );
+        }else{
+            datos.put("menssage","producto eliminado");
+            this.productRepository.deleteById(id);
+            return new ResponseEntity<>(
+                    datos,
+                    HttpStatus.ACCEPTED
+            );
+        }
+
+    }
+}
